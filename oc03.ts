c06236a54b450cad1db8c084068329209d6244df
brendanvanbreda <brendanvanbreda1993@gmail.com>
2018-09-07 14:41:58 +0200

    UPDATE SRC

--- oc03.ts
+++ oc03.ts
@@ -6,19 +6,18 @@ namespace OC03 {
     // PCA9536 Register Definitions
     const PCA9554A_I2C_ADDRESS = 0x38
     const PCA9554A_REG_INPUT_PORT = 0x00
-    const PCA9554A_REG_OUTPUT_PORT = 0x01	
+    const PCA9554A_REG_OUTPUT_PORT = 0x01
     const PCA9554A_REG_POL_INVERSION = 0x02
     const PCA9554A_REG_CONFIG = 0x03
     const PCA9554A_CONF_OUTPUT = 0x00
     const PCA9554A_CONF_INPUT = 0xFF
     const PCA9554A_ALL_OUTPUTS_OFF = 0x00
 
-     /**
-    * Used to write the desired output state of the OC03
-    * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 56
-    * @param state the state of the output channel
- 	*/
-
+    /**
+   * Used to write the desired output state of the OC03
+   * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 56
+   * @param state the state of the output channel
+     */
     function writeState(chipAddress: number = PCA9554A_I2C_ADDRESS, state: number): void {
         let buf: Buffer = pins.createBuffer(2);
         buf[0] = PCA9554A_REG_OUTPUT_PORT;
@@ -27,40 +26,45 @@ namespace OC03 {
     }
 
     /**
+   * Used to set the mode of the OC03
+   * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 56
+     */
+    function setModeOutput(chipAddress: number = PCA9554A_I2C_ADDRESS): void {
+        let buf: Buffer = pins.createBuffer(2);
+        buf[0] = PCA9554A_REG_CONFIG;
+        buf[1] = PCA9554A_CONF_OUTPUT;
+        pins.i2cWriteBuffer(chipAddress, buf, false);
+    }
+
+    /**
     * OC03 Init, used to reset the chip and switch all outputs low
     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 56
     * @param state the state of the output channel
  	*/
-    //% block
+    //% block"%on=toggleOnOff"
     //% weight=90
     export function init(chipAddress: number = PCA9554A_I2C_ADDRESS, state: boolean): void {
-        if(state) {
+        setModeOutput(chipAddress);
+        if (state) {
             writeState(chipAddress, 0x01);
         } else {
             writeState(chipAddress, 0x00);
         }
     }
-
-    /**
-     * Render a boolean as a on/off toggle
-     */
-    //% block="%on=toggleOnOff"
-    export function onOff(on: boolean): boolean {
-        return on;
-    }
-
+    
     /**
     * OC03 set the state of the output channel
     * @param chipAddress [64-125] The I2C address of your PCA9685; eg: 56
     * @param state the state of the output channel   
 	*/
-    //% block
+    //% block"%on=toggleOnOff"
     //% weight=90
     export function setState(chipAddress: number = PCA9554A_I2C_ADDRESS, on: boolean): void {
-        if(on) {
+        if (on) {
             writeState(chipAddress, 0x01);
         } else {
             writeState(chipAddress, 0x00);
         }
     }
 }
+
