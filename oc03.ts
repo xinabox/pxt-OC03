8722983d060dc34106ec536148dc126230198864
☒ Bjarke Gotfredsen <gotfredsen@users.noreply.github.com>
2019-09-11 17:38:20 +0200

    Updated to latest standard

--- oc03.ts
+++ oc03.ts
@@ -1,7 +1,8 @@
 /**
  * OC03 Relay Out Low Voltage
  */
-//% weight=99 color=#000000 icon="\uf205" block="OC03"
+//% weight=99 color=#444444 icon="\uf205" block="OC03"
+//% groups=['Functions', 'Optional']
 namespace OC03 {
     // PCA9536 Register Definitions
     const PCA9554A_REG_INPUT_PORT = 0x00
@@ -15,13 +16,22 @@ namespace OC03 {
     let OC03_I2C_ADDR = 0x38
 
     export enum OC03_ADDR {
-        Default = 0x38,
-        A0 = 0x39,
-        A1 = 0x3A,
-        A0_A1 = 0x3B,
-        A0_A2 = 0x3D,
-        A1_A2 = 0x3E,
-        A0_A1_A2 = 0x3F
+        //% block="Off Off Off"
+        Ax38 = 0x38,
+        //% block="Off Off On"
+        Ax39 = 0x39,
+        //% block="Off On  Off"
+        Ax3A = 0x3A,
+        //% block="Off On  On"
+        Ax3B = 0x3B,
+        //% block="On  Off Off"
+        Ax3C = 0x3C,
+        //% block="On  Off On"
+        Ax3D = 0x3D,
+        //% block="On  On  Off"
+        Ax3E = 0x3E,
+        //% block="On  On  On"
+        Ax3F = 0x3F
     }
 
     function writeState(state: number): void {
@@ -38,37 +48,29 @@ namespace OC03 {
         pins.i2cWriteBuffer(OC03_I2C_ADDR, buf, false);
     }
 
+    function init(): void {
+        setModeOutput();
+        writeState(0x00);
+    }
+
     /**
     * Used to set the Alternative address of the OC03
     * @param chipAddress [72-80] The I2C address of your PCA9536; eg: 72
     */
-    //% blockId="setAddress" block="set OC03 address %OC03_ADDR"
+    //% blockId="setAddress" block="OC03 set address %OC03_ADDR"
     //% weight=99
+    //% group="Optional"
     export function setAddress(OC03_ADDR: OC03_ADDR = 1): void {
         OC03_I2C_ADDR = OC03_ADDR;
-    }
-
-    /**
-    * OC03 Init, used to reset the chip and switch all outputs low
-    * @param state the state of the output channel
- 	*/
-    //% block"%on=toggleOnOff"
-    //% weight=90
-    export function init(state: boolean): void {
-        setModeOutput();
-        if (state) {
-            writeState(0x01);
-        } else {
-            writeState(0x00);
-        }
     }
 
     /**
     * OC03 set the state of the output channel
     * @param state the state of the output channel   
 	*/
-    //% block"%on=toggleOnOff" block="set device ouftput state %on"
+    //% block"%on=toggleOnOff" block="OC03 turn %on"
     //% weight=90
+    //% group="Functions"
     export function setState(on: boolean): void {
         if (on) {
             writeState(0x01);
@@ -76,4 +78,5 @@ namespace OC03 {
             writeState(0x00);
         }
     }
+    init();
 }
